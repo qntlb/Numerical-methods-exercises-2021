826bf78a74e47d69b7df9b3458902abeace16526
Andrea Mazzon <mazzon@math.lmu.de>
2021-06-18 07:37:50 +0200

    Solution to handout 9

---
+++ src/main/java/com/andreamazzon/exercise9/brownianmotion/package-info.java
@@ -0,0 +1,1 @@
+package com.andreamazzon.exercise9.brownianmotion;
---
+++ src/main/java/com/andreamazzon/exercise9/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * In this package we take care of a first, quite rough construction of a
+ * Brownian motion discretization and simulation. Moreover -and the Brownian
+ * motion construction is based on that- we write a class to wrap an array of
+ * doubles representing a sample of realizations of a random variable into an
+ * object standing for the random variable.
+ */
+package com.andreamazzon.exercise9;
